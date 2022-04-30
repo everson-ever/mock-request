ea330d5d5598830f8010c1f21bbd89a57b4360ea
Everson Silva <eversonsilva9799@gmail.com>
2022-04-30 19:29:09 -0300

    feat: add post route

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::RoutesController < ApplicationController
-  before_action :route_get!, only: [:index]
-  before_action :delay, only: [:index]
+  before_action :route_action!
+  before_action :delay
 
   def index
     render @route.render_type => @route.response_body,
            status: @route.status_code
   end
 
+  def store
+    render @route.render_type => @route.response_body,
+           status: @route.status_code
+  end
+
   private
 
-  def route_get!
+  def route_action!
     @route = route_action_service
 
     return render json: {}, status: :not_found if @route.blank?
