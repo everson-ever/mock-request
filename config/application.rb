6c94e7ff01a4b1ca79326a673b70dc9f2d49fad9
Everson Silva <eversonsilva9799@gmail.com>
2022-05-22 08:29:55 -0300

    feat: load envs

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
 module MockRequest
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
