910d64fb2876d28059672fc196f5e9a65e2f5a2a
Everson Silva <eversonsilva9799@gmail.com>
2022-05-22 14:48:52 -0300

    styles: rubocop

--- spec/factories/uploads.rb
+++ spec/factories/uploads.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     url_hash { SecureRandom.hex }
     filename { :filename }
     endpoint { "aaa/filename.png" }
-    file { 
+    file do
       Rack::Test::UploadedFile.new("spec/fixtures/files/less_than_10_mega.mp3")
-    }
+    end
   end
 end
